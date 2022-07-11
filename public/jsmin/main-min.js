c2e5ce1eba1c786b79adfce4156be89bbd2c65e7
unknown <Dmis0474@gmail.com>
2022-07-11 23:47:42 +0300

    finished adaptation

--- public/jsmin/main-min.js
+++ public/jsmin/main-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".options__items").slick({infinite:!0,slidesToShow:4,slidesToScroll:4})});
+$(document).ready(function(){$(".options__items").slick({infinite:!0,slidesToShow:4,slidesToScroll:4,responsive:[{breakpoint:1024,settings:{slidesToShow:3,slidesToScroll:3}},{breakpoint:600,settings:{slidesToShow:2,slidesToScroll:2}}]})});
--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,24 @@ $(document).ready(function(){
   $('.options__items').slick({
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 4
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+         
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+     
+    ]
     })
 });
